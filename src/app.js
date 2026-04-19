8da8ae02b6dec8d4781afd992fd648381f1e377e
malkiatpannu <malkiatpannu@gmail.com>
2026-04-19 10:58:18 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+// Health check
+app.get('/', (req, res) => {
+    res.json({ message: "API Running successfully" });
+});
+
+module.exports = app;
