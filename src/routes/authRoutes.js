ee2e9f191f29f56bb4aa833fe2825822fc3711f9
malkiatpannu <malkiatpannu@gmail.com>
2026-04-19 17:09:35 +0530

    Added validations to auth routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const validate = require('../middlewares/validate');
+const { registerSchema, loginSchema } = require('../validations/userValidation');
 const { register, login } = require('../controllers/authController');
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validate(registerSchema), register);
+router.post('/login', validate(loginSchema), login);
 
 module.exports = router;
